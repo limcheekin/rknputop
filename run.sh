83f6546a6dfb63d8ae3b943c7acd8bcc713a83e9
limcheekin <limcheekin@vobject.com>
2025-02-27 17:51:15 +0800

    chore: add virtual environment activation in run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# Check if the virtual environment is activated
+if [[ -z "$VIRTUAL_ENV" ]]; then
+    echo "Activating virtual environment..."
+    source .venv/bin/activate
+else
+    echo "Virtual environment already activated."
+fi
+
 sudo $(which python3) rknputop
